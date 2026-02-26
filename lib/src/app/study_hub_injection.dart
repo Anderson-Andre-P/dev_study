78e3a85b7ea04ce083226c9ada878128f816b833
Anderson-Andre-P <eleu.ander@gmail.com>
2026-02-26 10:47:17 -0300

    feat(app): wire clean architecture dependencies

---
+++ lib/src/app/study_hub_injection.dart
@@ -0,0 +1,11 @@
+import 'package:dev_study/src/features/study_hub/data/datasources/study_local_datasource.dart';
+import 'package:dev_study/src/features/study_hub/data/repositories/study_repository_impl.dart';
+
+import '../features/study_hub/domain/usecases/get_studies.dart';
+import '../features/study_hub/presentation/controllers/study_controller.dart';
+
+StudyController createStudyController() {
+  return StudyController(
+    GetStudies(StudyRepositoryImpl(StudyLocalDataSource())),
+  );
+}
