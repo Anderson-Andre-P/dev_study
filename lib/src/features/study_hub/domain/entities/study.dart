efd6cd08ff6ecc25746cd6f7a2b0a32fff4ad374
Anderson-Andre-P <eleu.ander@gmail.com>
2026-02-26 10:26:57 -0300

    feat(domain): introduce Study entity

---
+++ lib/src/features/study_hub/domain/entities/study.dart
@@ -0,0 +1,11 @@
+class Study {
+  final String title;
+  final String description;
+  final String icon;
+
+  const Study({
+    required this.title,
+    required this.description,
+    required this.icon,
+  });
+}
